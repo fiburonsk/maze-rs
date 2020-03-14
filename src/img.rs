165614bf88c8d88c13c984f0ed09b37572935edb
fiburonsk <10441670+fiburonsk@users.noreply.github.com>
2020-03-14 16:44:18 -0500

    fix update to image crate

--- src/img.rs
+++ src/img.rs
@@ -4,11 +4,11 @@ use image;
 pub fn save(maze: &Maze, solution: &[Pos], name: &str) {
     let imgx = maze.width() as u32;
     let imgy = maze.height() as u32;
-    let wall = image::Rgb([128, 128, 128]);
-    let path = image::Rgb([48, 48, 48]);
-    let start = image::Rgb([0, 255, 0]);
-    let finish = image::Rgb([255, 0, 0]);
-    let visit = image::Rgb([225, 200, 128]);
+    let wall: image::Rgb<u8> = image::Rgb([128, 128, 128]);
+    let path: image::Rgb<u8> = image::Rgb([48, 48, 48]);
+    let start: image::Rgb<u8> = image::Rgb([0, 255, 0]);
+    let finish: image::Rgb<u8> = image::Rgb([255, 0, 0]);
+    let visit: image::Rgb<u8> = image::Rgb([225, 200, 128]);
 
     let mut buf = image::ImageBuffer::new(imgx, imgy);
 
@@ -19,14 +19,12 @@ pub fn save(maze: &Maze, solution: &[Pos], name: &str) {
                 y: y as usize,
             };
             let p = buf.get_pixel_mut(x as u32, y as u32);
-            let c = match &maze.at(&pos) {
+            *p = match &maze.at(&pos) {
                 Part::Wall => wall,
                 Part::Start => start,
                 Part::Finish => finish,
                 Part::Open => path,
             };
-
-            *p = c;
         });
     });
 
